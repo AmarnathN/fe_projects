cc68964938e00b84badf44828c1271e4413d8440
AmarnathN <vayuteja@gmail.com>
2024-01-22 00:09:17 +0530

    Add mongoose DB

--- real_estate/api/index.js
+++ real_estate/api/index.js
@@ -1,8 +1,19 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(
+    "mongodb+srv://vayuteja:vayuteja@rela-estate.hcmjkgs.mongodb.net/real-estate?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log("Api running on 3000 ! ")
-})
+app.listen(3000, () => {
+  console.log("Api running on 3000 ! ");
+});
