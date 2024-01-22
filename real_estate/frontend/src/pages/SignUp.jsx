6497e1f168f5bde423b0da522f2e8618ddf66fa8
AmarnathN <vayuteja@gmail.com>
2024-01-22 21:01:06 +0530

    Add signup Fe changes

--- real_estate/frontend/src/pages/SignUp.jsx
+++ real_estate/frontend/src/pages/SignUp.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className="max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-10">SignUp</h1>
+      <form className="flex flex-col gap-2">
+        <input
+          type="text"
+          placeholder="Username"
+          className="border p-3 rounded-lg m-1"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="border p-3 rounded-lg m-1"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-3 rounded-lg m-1"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg m-1 uppercase">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-2">
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-500"> Sign in </span>
+        </Link>
+      </div>
+    </div>
+  );
 }
